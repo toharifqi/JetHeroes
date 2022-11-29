8fc189e7b88d5722f3ff5e3ffbcd1fd303344bee
Rifqi Naufal <rifqi.nufaltohari@gojek.com>
2022-11-29 08:58:53 +0700

    Add scroll to top button

--- app/src/main/java/com/toharifqi/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/toharifqi/jetheroes/JetHeroesApp.kt
@@ -1,19 +1,39 @@
 package com.toharifqi.jetheroes
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,13 +41,22 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.toharifqi.jetheroes.model.HeroesData
 import com.toharifqi.jetheroes.ui.theme.JetHeroesTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun JetHeroesApp(
     modifier: Modifier = Modifier
 ) {
     Box(modifier = modifier) {
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val isButtonShown: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp)
+        ) {
             items(HeroesData.heroes, key = { it.id }) { hero ->
                 HeroListItem(
                     name = hero.name,
@@ -35,6 +64,20 @@ fun JetHeroesApp(
                     modifier = Modifier.fillMaxWidth()
                 )
             }
+        }
+        AnimatedVisibility(
+            visible = isButtonShown,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(onClick = {
+                scope.launch {
+                    listState.scrollToItem(index = 0)
+                }
+            })
         }
     }
 }
@@ -84,6 +127,29 @@ fun HeroListItemPreview() {
         HeroListItem(
             name = "H.O.S.Cokroaminoto",
             photoUrl = ""
+        )
+    }
+}
+
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .shadow(elevation = 10.dp, shape = CircleShape)
+            .clip(shape = CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White,
+            contentColor = MaterialTheme.colors.primary
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = null
         )
     }
 }
